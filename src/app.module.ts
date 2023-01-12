cb0f219271c7716f234a9e63e13cfc37fb6a4237
ThiagoLopezA <thiagolopezalderete@hotmail.com>
2023-01-12 20:49:24 -0300

    feat(database) : typeorm configured

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { enviroments } from './enviroments';
+import { DatabaseModule } from './database/database.module';
+import config from './config';
 
 @Module({
-  imports: [],
+  imports: [
+    DatabaseModule,
+    ConfigModule.forRoot({
+      load: [config],
+      envFilePath: enviroments[process.env.NODE_ENV] || '.env',
+      isGlobal: true,
+      validationSchema: Joi.object({
+        POSTGRES_DB: Joi.string().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_HOST: Joi.string().required(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
