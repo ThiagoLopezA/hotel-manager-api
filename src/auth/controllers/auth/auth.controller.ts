68c1a87e0cb5d342a03b4701a61d546b2211dd24
Thiago López <thiagolopezalderete@hotmail.com>
2023-03-18 10:52:25 -0300

    fix: auth controller added to swagger docs

--- src/auth/controllers/auth/auth.controller.ts
+++ src/auth/controllers/auth/auth.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiTags } from '@nestjs/swagger';
 import { Request } from 'express';
 import { AuthService } from 'src/auth/services/auth/auth.service';
 import { User } from 'src/users/entities/user.entity';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
